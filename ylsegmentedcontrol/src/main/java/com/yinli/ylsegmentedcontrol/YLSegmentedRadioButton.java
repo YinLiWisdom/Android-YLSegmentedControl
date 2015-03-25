ae1f137c04975784363f7ab94793187b2e410d8e
Yin Li <li.yinmax@outlook.com>
2015-03-25 01:27:51 +0000

    Version 1

--- ylsegmentedcontrol/src/main/java/com/yinli/ylsegmentedcontrol/YLSegmentedRadioButton.java
+++ ylsegmentedcontrol/src/main/java/com/yinli/ylsegmentedcontrol/YLSegmentedRadioButton.java
@@ -1,8 +1,14 @@
 package com.yinli.ylsegmentedcontrol;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RadioButton;
 
 /**
@@ -11,28 +17,47 @@ import android.widget.RadioButton;
 public class YLSegmentedRadioButton extends RadioButton {
 
     public YLSegmentedRadioButton(Context context) {
-        this(context, null);
+        super(context);
     }
 
     public YLSegmentedRadioButton(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
     }
 
     public YLSegmentedRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
+    /*    public YLSegmentedRadioButton(Context context) {
+        super(context, null);
+    }
+
+    public YLSegmentedRadioButton(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public YLSegmentedRadioButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }*/
+
     private void init() {
 
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        updateButtonAppearance();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    private void updateButtonAppearance() {
+        Resources res = getResources();
+
+        float verticalPadding = res.getDimension(R.dimen.default_button_vertical_padding);
+        float horizontalPadding = res.getDimension(R.dimen.default_button_horizontal_padding);
+        setPadding((int) horizontalPadding, (int) verticalPadding, (int) horizontalPadding, (int) verticalPadding);
+        setMinHeight((int) res.getDimension(R.dimen.default_button_min_height));
+        setGravity(Gravity.CENTER);
+        setButtonDrawable(null);
     }
 }
--- ylsegmentedcontrol/src/main/java/com/yinli/ylsegmentedcontrol/YLSegmentedRadioGroup.java
+++ ylsegmentedcontrol/src/main/java/com/yinli/ylsegmentedcontrol/YLSegmentedRadioGroup.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 
 /**
@@ -17,7 +24,13 @@ public class YLSegmentedRadioGroup extends RadioGroup {
     /* Color attributes */
     private int mCheckedTextColor, mDisabledTextColor, mNormalTextColor, mPressedTextColor;
     private int mCheckedBackgroundColor, mDisabledBackgroundColor, mNormalBackgroundColor, mPressedBackgroundColor;
+    private int mBorderColor;
 
+    private Resources res;
+
+    private enum ButtonType {
+        LEFT, TOP, MIDDLE, RIGHT, BOTTOM, SINGLE
+    }
 
     public YLSegmentedRadioGroup(Context context) {
         this(context, null);
@@ -25,17 +38,30 @@ public class YLSegmentedRadioGroup extends RadioGroup {
 
     public YLSegmentedRadioGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-
+        res = getResources();
+        loadDefaultValues();
+        init();
     }
 
     private void loadDefaultValues() {
-
+        loadDefaultColors();
     }
 
     private void loadDefaultColors() {
-        Resources res = getResources();
-        mCheckedTextColor = res.getColor(R.color.default_text_color_normal);
+        /* Load default colors for text */
+        mCheckedTextColor = res.getColor(R.color.default_text_color_checked);
+        mDisabledTextColor = res.getColor(R.color.default_text_color_disabled);
+        mNormalTextColor = res.getColor(R.color.default_text_color_normal);
+        mPressedTextColor = res.getColor(R.color.default_text_color_pressed);
+
+        /* Load default colors for background */
+        mCheckedBackgroundColor = res.getColor(R.color.default_background_color_checked);
+        mDisabledBackgroundColor = res.getColor(R.color.default_background_color_disabled);
+        mNormalBackgroundColor = res.getColor(R.color.default_background_color_normal);
+//        mPressedBackgroundColor = Color.parseColor("#99" + res.getColor(R.color.default_background_color_checked));
+        mPressedBackgroundColor = res.getColor(R.color.default_background_color_pressed);
+
+        mBorderColor = res.getColor(R.color.default_border_color);
     }
 
     private void init() {
@@ -45,36 +71,101 @@ public class YLSegmentedRadioGroup extends RadioGroup {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        updateButtonAppearance();
-    }
-
-    public void updateButtonAppearance() {
         int count = getChildCount();
         if (count == 1) {
-
+            updateButtonAppearance(getChildAt(0), ButtonType.SINGLE);
         } else if (count > 1) {
-
+            int orientaion = getOrientation();
+            if (orientaion == LinearLayout.HORIZONTAL) {
+                updateButtonAppearance(getChildAt(0), ButtonType.LEFT);
+                updateButtonAppearance(getChildAt(count - 1), ButtonType.RIGHT);
+            } else {
+                updateButtonAppearance(getChildAt(0), ButtonType.TOP);
+                updateButtonAppearance(getChildAt(count - 1), ButtonType.BOTTOM);
+            }
+            for (int i = 1; i < count - 1; i++) {
+                updateButtonAppearance(getChildAt(i), ButtonType.MIDDLE);
+            }
         }
     }
 
-    private void updateButtonText(View view, Color color) {
-        int[][] states = new int[][] {
-                new int[] {-android.R.attr.state_checked}, // normal
-                new int[] {-android.R.attr.state_enabled}, // disabled
-                new int[] { android.R.attr.state_checked}, // checked
-                new int[] { android.R.attr.state_pressed}  // pressed
-        };
-
-        Resources res = getResources();
-        int[] colors = new int[] {
-
-        };
-
-        ColorStateList myList = new ColorStateList(states, colors);
+    public void updateButtonAppearance(View view, ButtonType type) {
+        updateButtonText(view);
+        updateButtonBackground(view, type);
+        updateIcon(view);
     }
 
-    private void updateButtonBackground() {
-        GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[]{Color.RED, Color.GREEN});
-        gd.setStroke(10, Color.BLUE);
+    private void updateIcon(View view) {
+        Drawable[] drawables = ((Button) view).getCompoundDrawables();
+        for (int i = 0; i < drawables.length; i++) {
+            if (drawables[i] != null) {
+                drawables[i].setColorFilter(mNormalTextColor, PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+        ((Button) view).setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], drawables[2], drawables[3]);
     }
+
+    private void updateButtonText(View view) {
+        int[][] states = new int[][]{
+                {-android.R.attr.state_checked},
+                {-android.R.attr.state_enabled},
+                {android.R.attr.state_checked},
+                {android.R.attr.state_pressed}
+        };
+
+        int[] colors = new int[]{
+                mNormalTextColor,
+                mDisabledTextColor,
+                mCheckedTextColor,
+                mPressedTextColor
+        };
+
+        ColorStateList colorStateList = new ColorStateList(states, colors);
+        ((Button) view).setTextColor(colorStateList);
+    }
+
+    private void updateButtonBackground(View view, ButtonType type) {
+
+        GradientDrawable normalDrawable = createDrawable(type);
+        GradientDrawable disabledDrawable = createDrawable(type);
+        GradientDrawable pressedDrawable = createDrawable(type);
+        GradientDrawable checkedDrawable = createDrawable(type);
+
+        normalDrawable.setColor(mNormalBackgroundColor);
+        disabledDrawable.setColor(mDisabledBackgroundColor);
+        pressedDrawable.setColor(mPressedBackgroundColor);
+        checkedDrawable.setColor(mCheckedBackgroundColor);
+
+        StateListDrawable stateListDrawable = new StateListDrawable();
+        stateListDrawable.addState(new int[]{-android.R.attr.state_checked}, normalDrawable);
+        stateListDrawable.addState(new int[]{-android.R.attr.state_enabled}, disabledDrawable);
+        stateListDrawable.addState(new int[]{android.R.attr.state_checked}, checkedDrawable);
+        stateListDrawable.addState(new int[]{android.R.attr.state_pressed}, pressedDrawable);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackgroundDrawable(stateListDrawable);
+        } else {
+            view.setBackground(stateListDrawable);
+        }
+    }
+
+    private GradientDrawable createDrawable(ButtonType type) {
+
+        float radius = res.getDimension(R.dimen.default_corner_radius);
+        GradientDrawable drawable = (GradientDrawable) res.getDrawable(R.drawable.button_background).mutate();
+        if (type == ButtonType.LEFT) {
+            drawable.setCornerRadii(new float[]{radius, radius, 0, 0, 0, 0, radius, radius});
+        } else if (type == ButtonType.RIGHT) {
+            drawable.setCornerRadii(new float[]{0, 0, radius, radius, radius, radius, 0, 0});
+        } else if (type == ButtonType.SINGLE) {
+            drawable.setCornerRadii(new float[]{radius, radius, radius, radius, radius, radius, radius, radius});
+        } else if (type == ButtonType.TOP) {
+            drawable.setCornerRadii(new float[]{radius, radius, radius, radius, 0, 0, 0, 0});
+        } else if (type == ButtonType.BOTTOM) {
+            drawable.setCornerRadii(new float[]{0, 0, 0, 0, radius, radius, radius, radius});
+        }
+
+        return drawable;
+    }
+
 }
